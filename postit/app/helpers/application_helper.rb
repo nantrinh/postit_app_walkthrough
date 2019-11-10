8cd130cf8d131e8f90897a4733baaf220d557a7a
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-09 22:34:57 -0800

    [REMOVE]: postit

--- postit/app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  def fix_url(str)
-    str.starts_with?("http://") ? str : "http://#{str}"
-  end
-
-  def display_datetime(dt)
-    dt.strftime("%m/%d/%Y %l:%M%P %Z")
-  end
-end
