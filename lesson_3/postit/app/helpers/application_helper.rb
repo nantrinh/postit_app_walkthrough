8b078b53db0620aa1fa1c376e969d567ac142df1
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-10 16:06:28 -0800

    [FIX]: do not display http://https://

--- lesson_3/postit/app/helpers/application_helper.rb
+++ lesson_3/postit/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def fix_url(str)
-    str.starts_with?("http://") ? str : "http://#{str}"
+    str.start_with?('http://', 'https://') ? str : "http://#{str}"
   end
 
   def display_datetime(dt)
