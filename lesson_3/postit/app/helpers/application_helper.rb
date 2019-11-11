ec1b2f8291a2aa45dbdd35b11298a7f88a73c0dd
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-10 16:33:26 -0800

    [STYLES]: make text fit inside post cards

--- lesson_3/postit/app/helpers/application_helper.rb
+++ lesson_3/postit/app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def display_datetime(dt)
     dt.strftime("%m/%d/%Y %l:%M%P %Z")
   end
+
+  def restrict_newlines(text, n)
+    text.count("\n") > n ? text.split("\n")[0..n].join("\n") + "..." : text
+  end
 end
