d7a868cc4b4f63322a413368f5cf7833b8b3900c
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-13 11:52:42 -0800

    [UPDATE]: error message when not logged in

--- lesson_3/postit/app/controllers/application_controller.rb
+++ lesson_3/postit/app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:error] = "Must be logged in to do that."
+      flash[:error] = "You must be logged in to do that."
       redirect_to root_path
     end
   end
