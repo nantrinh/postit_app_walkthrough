5700081efcac656b3f91d9d7d4c9d632af7b6167
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-13 20:34:04 -0800

    [ADD]: allow user to change their mind for comment votes

--- lesson_3/postit/app/controllers/comments_controller.rb
+++ lesson_3/postit/app/controllers/comments_controller.rb
@@ -19,7 +19,12 @@ class CommentsController < ApplicationController
 
   def vote
     @comment = Comment.find(params[:id])
-    Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+    existing_votes = @comment.votes.where(user_id: current_user.id)
+    if existing_votes.empty?
+      Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
+    else
+      existing_votes.first.update(vote: params[:vote])
+    end
     redirect_back fallback_location: root_path
   end
 
