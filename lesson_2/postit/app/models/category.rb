d3121ecefdbdb729eb4d6b35a0a45cfa81076ed4
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-20 12:28:10 -0800

    [REMOVE]: postit directories

--- lesson_2/postit/app/models/category.rb
+++
@@ -1,6 +0,0 @@
-class Category < ApplicationRecord
-  has_many :post_categories, dependent: :destroy
-  has_many :posts , through: :post_categories
-
-  validates :name, presence: true, uniqueness: true
-end
--- lesson_2/postit/app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-end
